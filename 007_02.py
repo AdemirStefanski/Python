686d1079b481513fa2622346d2e2c1217f20313e
Ademir Stefanski <ademirstefanski@hotmail.com>
2022-10-31 15:19:17 -0300

    Usando classe, método e função para criar uma calculadora onde o usuário seleciona a operação a ser executada através de um case.

---
+++ 007_02.py
@@ -0,0 +1,40 @@
+num1 = int(input('Entre com o valor A: '))
+num2 = int(input('Entre com o valor B: '))
+
+class Calculadora:
+    def __init__(self):
+        pass
+
+    def soma(self, valor_a, valor_b):
+        return valor_a + valor_b
+
+    def subtracao(self, valor_a, valor_b):
+        return valor_a - valor_b
+
+    def multiplicacao(self, valor_a, valor_b):
+        return valor_a * valor_b
+
+    def divisao(self, valor_a, valor_b):
+        return valor_a / valo
+
+calculadora = Calculadora()
+
+lista = [1, 2, 3, 4]
+operacao = ''
+#operacao = input('\nQual operação deseja realizar?\n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n\nOperação: ')
+
+while operacao not in lista:
+    operacao = int(input('\nQual operação deseja realizar?\n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n\nOperação: '))
+
+operacao = str(operacao)
+
+match operacao:
+    case '1':
+        print('\nSoma: {}' .format(calculadora.soma(num1, num2)))
+    case '2':
+        print('\nSubtração: {}' .format(calculadora.subtracao(num1,num2)))
+    case '3':
+        print('\nMultiplicação: {}' .format(calculadora.multiplicacao(num1,num2)))
+    case '4':
+        print('\n Divisão: {}' .format(calculadora.divisao(num1,num2)))
+
